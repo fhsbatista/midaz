a64b400f17d97f3b9d03f8f48f5a6ce05427312d
Fernando Batista <fernandohsbatista@gmail.com>
2025-02-17 15:07:20 -0300

    test: withBody ValidateStruct coverage :rotating_light:

--- pkg/net/http/withBody_test.go
+++ pkg/net/http/withBody_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/LerianStudio/midaz/pkg"
+	gold "github.com/LerianStudio/midaz/pkg/gold/transaction/model"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/stretchr/testify/require"
@@ -142,4 +143,158 @@ func TestParseUUIDPathParameters_ValidAndInvalidUUID(t *testing.T) {
 	require.NoError(t, err)
 
 	require.Equal(t, fiber.StatusBadRequest, resp.StatusCode)
+}
+
+func TestValidateStruct(t *testing.T) {
+	t.Run("Valid Struct", func(t *testing.T) {
+		type ValidStruct struct {
+			Key   string `validate:"valuemax=10"`
+			Value string `validate:"valuemax=20"`
+		}
+
+		validInput := ValidStruct{
+			Key:   "validKey",
+			Value: "validValue",
+		}
+
+		err := ValidateStruct(validInput)
+
+		require.NoError(t, err, "expected no error for a valid struct")
+	})
+
+	t.Run("Required fields missing", func(t *testing.T) {
+		type InvalidStruct struct {
+			Name string `json:"name" validate:"required"`
+		}
+
+		invalidInput := InvalidStruct{
+			Name: "",
+		}
+		err := ValidateStruct(invalidInput)
+
+		require.Error(t, err, "expected an error warning required fields are missing")
+	})
+
+	t.Run("Key length exceeded", func(t *testing.T) {
+		type InvalidStruct struct {
+			Key   string `validate:"keymax=5"`
+			Value string
+		}
+	
+		invalidInput := InvalidStruct{
+			Key:   "exceedingKeyLength",
+			Value: "validValue",
+		}
+	
+		err := ValidateStruct(invalidInput)
+	
+		require.Error(t, err, "expected an error for exceeding key length")
+	})
+
+	t.Run("Value length exceeded", func(t *testing.T) {
+		type InvalidStruct struct {
+			Key   string
+			Value string `validate:"valuemax=20"`
+		}
+	
+		invalidInput := InvalidStruct{
+			Key:   "valid",
+			Value: "exceedingValueMaxCharsLength",
+		}
+	
+		err := ValidateStruct(invalidInput)
+	
+		require.Error(t, err, "expected an error for exceeding value length")
+	})
+
+	t.Run("No Nested field is nested", func(t *testing.T) {
+		type InvalidStruct struct {
+			Name     string         `json:"name"`
+			Metadata map[string]any `json:"metadata" validate:"nonested"`
+		}
+	
+		invalidInput := InvalidStruct{
+			Name: "valid name",
+			Metadata: map[string]any{
+				"value1": "Value 1",
+				"value2": "Value 2",
+				"value3": "Value 3",
+			},
+		}
+	
+		err := ValidateStruct(invalidInput)
+	
+		require.Error(t, err, "expected an error for nesting a map")
+	})
+
+	t.Run("Transacation with more than one FromTo", func(t *testing.T) {
+		type InvalidStruct struct {
+			Amount int64         `json:"amount"`
+			To     []gold.FromTo `json:"to" validate:"singletransactiontype"`
+		}
+	
+		validFromTo := makeFromTo().
+			addAmount(200).
+			FromTo
+	
+		invalidFromTo := makeFromTo().
+			addAmount(200).
+			addShare(50).
+			addRemaining("test").
+			FromTo
+	
+		invalidInput := InvalidStruct{
+			Amount: 200.0,
+			To: []gold.FromTo{
+				validFromTo,
+				invalidFromTo,
+			},
+		}
+	
+		err := ValidateStruct(invalidInput)
+	
+		require.Error(t, err, "expected an error for transaction containing more than one transaction type (Amount, Share, Remaining)")
+	})
+}
+
+type FromToExtension struct {
+	FromTo gold.FromTo
+}
+
+func makeFromTo() *FromToExtension {
+	return &FromToExtension{
+		FromTo: gold.FromTo{
+			Account:         "account",
+			Rate:            nil,
+			Description:     "description",
+			ChartOfAccounts: "chart",
+			Metadata:        nil,
+			IsFrom:          true,
+		},
+	}
+}
+
+func (ftx *FromToExtension) addAmount(value int64) *FromToExtension {
+	ftx.FromTo.Amount = &gold.Amount{
+		Asset: "BRL",
+		Value: value,
+		Scale: 2,
+	}
+
+	return ftx
+}
+
+func (ftx *FromToExtension) addShare(value int64) *FromToExtension {
+	ftx.FromTo.Share = &gold.Share{
+		Percentage:             value,
+		PercentageOfPercentage: value,
+	}
+
+	return ftx
+}
+
+func (ftx *FromToExtension) addRemaining(value string) *FromToExtension {
+	ftx.FromTo.Remaining = value
+
+	return ftx
 }
